9832f92dae7536e381d887d9defe83b562551d6b
BOBkumo <leeseungyun123@gmail.com>
2023-07-24 23:51:20 +0900

    update init_permutation_code

--- 전치암호.py
+++ 전치암호.py
@@ -3,18 +3,21 @@ import math
 def Transposition_Ciper(data, key):
     text_1 = ''
     num_columns = len(key)
-    num_rows = (len(data) // num_columns)
+    num_rows = (len(data) // num_columns) #키 값이 평문에 접근하는 블록의 크기를 나타냄.
     print(num_columns)
     data_list = list(data)
 
     #DREAMHACK DRE AMH ACK
     #          
 
-    for i in range(num_rows):
-        for j in range(num_columns):
+    for i in range(num_columns):
+        for j in range(num_rows):
+            #DREAMHACK , [3,1,2]일때.
+            #i * num_columns 는 j 가 반복되는동안 각 블록의 첫번째 인덱스를 가리킴. 즉, i = 0 일 때, DRE를 전치시킴.
+            #index = 0(D) + 3 - 1 = 2. INDEX = 2, data_list[2] = E를 text_1에 넣음.
+            #i = 1 일 때, j가 반복되는 동안 두번 째 블록인 AMH를 전치시킴.
             index = i * num_columns + key[j] - 1
             text_1 += data_list[index]
-
     return text_1, num_rows
 
 input_data = input("평문을 입력해주세요.")
