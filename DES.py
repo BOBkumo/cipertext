a2927b6176d6abe7ab48be077875fdfd806d2c96
BOBkumo <leeseungyun123@gmail.com>
2023-07-31 20:06:14 +0900

    update_des_file

--- DES.py
+++ DES.py
@@ -50,59 +50,59 @@ EPT = [32, 1, 2, 3, 4, 5,
 S_box = [
     # S1  
     [
-        14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
-        0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
-        4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
-        15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+        [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+        [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
     ],
     # S2
     [
-        15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
-        3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
-        0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
-        13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9
+        [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+        [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+        [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+        [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
     ],
     # S3
     [
-        10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
-        13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
-        13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
-        1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12
+        [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+        [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+        [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+        [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
     ],
     #S4
     [
-        7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
-        13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
-        10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
-        3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14
+        [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+        [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+        [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+        [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
     ],
     #S5
     [
-        2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
-        14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
-        4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
-        11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3
+        [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+        [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+        [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+        [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
     ],
     #S6
     [
-        12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
-        10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
-        9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
-        4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13
+        [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+        [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+        [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+        [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
     ],
     #S7
     [
-        4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
-        13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
-        1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
-        6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12
+        [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+        [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+        [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+        [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
     ],
     #S8
     [
-        13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
-        1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
-        7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
-        2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11
+        [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+        [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+        [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+        [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
     ]
 ]
 
@@ -112,10 +112,15 @@ SPT = [16, 7, 20, 21, 29, 12, 28, 17,
        19, 13, 30, 6, 22, 11, 4, 25]
 
 def s_box_output(s_box_number, input_bit):
-    row = (input_bit[0] << 1) + input_bit[5]
-    column = (input_bit[1] << 3) + (input_bit[2] << 2) + (input_bit[3] << 1) + input_bit[4]
-    output = S_box[s_box_number][16 * row + column]
-    return bytearray([int(bit) for bit in format(output, '04b')])
+    output = []
+
+    row = (s_box_number[0] << 1) + s_box_number[5]
+    column = (s_box_number[1] << 3) + (s_box_number[2] << 2) + (s_box_number[3] << 1) + s_box_number[4]
+
+    output = input_bit[row][column]
+    binary = bin(output)[2:].zfill(4)
+
+    return bytearray([int(bit) for bit in binary])
 
 def change_to_bit(plain: str):
     bit2 = ''
@@ -158,7 +163,7 @@ def bitarray_to_plain(bitarray: bytearray):
 
 def permutation(text: bytearray, table: list[int], length: int):
     permutated_text = bytearray(length)
-
+    
     for num in range(len(table)):
         point = table[num]-1     #IP_table은 1부터 시작하지만 text의 인덱스는 0부터 시작하기때문에 -1 수행
         permutated_text[num] = text[point]
@@ -187,7 +192,6 @@ def key_made(key: bytearray):
             left = left[2:] + left[0:2]
             right = right[2:] + right[0:2]
         round_key.append(permutation(left+right, CPBT, 48))
-    print(round_key)
     return round_key
 
 def round_func(result, round_key):
@@ -206,27 +210,25 @@ def round_func(result, round_key):
             s_box_work[4*block_index:4*block_index+4] = s_box_output(expansion[6*block_index:6*block_index+6], S_box[block_index])
 
         strighted = permutation(s_box_work, SPT, 32)
+        '''
+        bytearray 자료형의 strighted 와 left_plain을 xor 연산하기 위해 bytes의 자료형으로 전환 후 xor 연산 진행.
+        '''
+        right_plain = bytearray(a ^ b for a,b in zip(left_plain, strighted))
+    '''
+    round함수 종료 후 right_plain 32bit 와 left_plain 32bit를 더해 최종 결과값을 도출.
+    그 후 FPT 에 넣어 순열을 진행시킨다.
+    '''
+    round_func_result = right_plain + left_plain
+    return round_func_result
 
-
-'''
-def is_2d_array(lst):
-    if not isinstance(lst, list):
-        return False
-
-    for item in lst:
-        if not isinstance(item, list):
-            return False
-
-    return True
-'''
 
 key = input("key 값을 입력해라. : ")
 key_bitarray = change_to_bit(key)
 key_bitarray1 = bit_to_bitarray(key_bitarray)
 round_key = key_made(key_bitarray1)
 round_key_to_bit = bitarray_to_bit(round_key)
-print(round_key_to_bit)
-print(type(round_key_to_bit))
+#print(round_key_to_bit)
+#print(type(round_key_to_bit))
 #print(is_2d_array(round_key))
 
 text = input("평문을 입력해라. : ")
@@ -237,9 +239,7 @@ print(f"bytearray 변경문 출력: {bit_to_bitarray(bitarray)}")
 print("길이: ", len(bytearray1))
 '''
 ip_work = permutation(bytearray1, IPT, 64)
-round_func_work = round_func(ip_work, key_bitarray1)
+round_func_work = round_func(ip_work, round_key)
+des_result = permutation(round_func_work, FPT, 64)
 result = bitarray_to_bit(ip_work)
 print("실행결과: ", result)
-print(len(result))
-print(type(result))
-print(bitarray)
